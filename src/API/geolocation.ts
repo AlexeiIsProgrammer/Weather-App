39605aeba09003cce90cd0e6a8c836e6a29f2430
Alexei <avenge.now.r@mail.ru>
2023-08-24 21:15:35 +0300

    feat: add prod

--- src/API/geolocation.ts
+++ src/API/geolocation.ts
@@ -3,7 +3,7 @@ import { Geolocation } from '../models/geolocation';
 
 export default async function getPosition() {
   const response = await axios.get<Geolocation>(
-    `https://ipinfo.io/json?token=${process.env.REACT_APP_POSITION_TOKEN}`
+    `https://ipinfo.io/json?token=${process.env.REACT_APP_POSITION_TOKEN}`,
   );
 
   return response.data.city;
