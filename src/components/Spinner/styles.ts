f1a481e08c716ce7a7a250ee64f3210e8dab3ff2
Alexei <avenge.now.r@mail.ru>
2023-09-28 20:07:07 +0300

    refactor: add loader position

--- src/components/Spinner/styles.ts
+++ src/components/Spinner/styles.ts
@@ -4,7 +4,7 @@ export const SpinnerAnimation = styled.img`
   height: 100%;
   width: 100%;
   z-index: -1;
-  position: relative;
+  position: fixed;
   background-image: url(./assets/spinning-cat.gif);
   background-repeat: no-repeat;
   background-size: contain;
